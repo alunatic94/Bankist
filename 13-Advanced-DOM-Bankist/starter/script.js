c813a30f24e9fb76730ad348f7171e0b8fcbd837
alunatic94 <Alberto.luna.642@my.csun.edu>
2021-07-28 22:18:34 -0700

    added a cookie message

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -7,8 +7,18 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-
-const openModal = function () {
+const message=document.createElement('div');
+const header=document.querySelector('.header');
+message.classList.add('cookie-message');
+message.innerHTML=
+'We use cookies to improve performance <button class="btn btn--close-cookie">Got it!</button>';
+header.prepend(message);
+document.querySelector('.btn--close-cookie').
+addEventListener('click',function(){
+  message.remove();
+});
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -17,9 +27,9 @@ const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
-
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click',openModal));
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
